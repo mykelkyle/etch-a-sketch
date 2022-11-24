feeef7911cb00e7f10d8c0da0a0a26949d89acf6
Mykel Murphy <murphy@akane.waseda.jp>
2022-11-24 19:28:58 +0900

    add and style rest of html elements

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-// function drawing() {
-//   cell.addEventListener("mouseover", function () {
-//     cell.classList.add("cellDown");
-//   });
-// }
-
-// container.addEventListener("mousedown", function () {
-//   drawing, true;
-// });
-
-// container.addEventListener("mouseup", function () {
-//   drawing, false;
-// });
-
 // creates 16x16 grid of divs, appends to container and adds class of "cell"
 
 let drawing = false;
-const container = document.getElementById("container");
+const container = document.getElementById("grid-container");
 
 for (let i = 0; i < 256; i++) {
   const cell = document.createElement("div");
