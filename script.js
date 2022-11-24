15657a740c2cdec3da0090cbcf6aa9335d76ee2c
Mykel Murphy <murphy@akane.waseda.jp>
2022-11-24 18:24:05 +0900

    add "drawing" functionality
    Add mousedown, mouseover, mouseup event listeners to allow user to click and drag to draw

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+// function drawing() {
+//   cell.addEventListener("mouseover", function () {
+//     cell.classList.add("cellDown");
+//   });
+// }
+
+// container.addEventListener("mousedown", function () {
+//   drawing, true;
+// });
+
+// container.addEventListener("mouseup", function () {
+//   drawing, false;
+// });
+
+// creates 16x16 grid of divs, appends to container and adds class of "cell"
+
+let drawing = false;
 const container = document.getElementById("container");
 
 for (let i = 0; i < 256; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
-  container.appendChild(cell);
 
-  cell.addEventListener("mousemove", function () {
-    cell.classList.add("cellDown");
+  cell.addEventListener("mousedown", () => {
+    drawing = true;
   });
+  cell.addEventListener("mouseup", () => {
+    drawing = false;
+  });
+  cell.addEventListener("mouseover", () => {
+    if (drawing === true) {
+      cell.classList.add("cellDown");
+    }
+  });
+
+  container.appendChild(cell);
 }
